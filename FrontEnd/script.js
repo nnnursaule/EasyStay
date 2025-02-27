a0681bf4c4f5ce345b87389f45718e08535703f1
nnnursaule <120462317+nnnursaule@users.noreply.github.com>
2025-02-27 23:37:27 +0500

    Update script.js

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -17,3 +17,32 @@ document.querySelectorAll(".faq-header").forEach((item) => {
         button.textContent = faqItem.classList.contains("active") ? "✖" : "+";
     });
 });
+
+
+
+
+// login
+
+document.getElementById("openModal").addEventListener("click", function() {
+    document.getElementById("loginModal").style.display = "flex";
+});
+
+document.querySelector(".close").addEventListener("click", function() {
+    document.getElementById("loginModal").style.display = "none";
+});
+
+function openTab(evt, tabName) {
+    let tabcontent = document.getElementsByClassName("tabcontent");
+    let tablinks = document.getElementsByClassName("tablinks");
+
+    for (let i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    for (let i = 0; i < tablinks.length; i++) {
+        tablinks[i].classList.remove("active");
+    }
+
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.classList.add("active");
+}
