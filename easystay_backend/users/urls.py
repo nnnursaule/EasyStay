c2d98901d0e0a32c0eb0e0b70223f2339923579d
yera72934 <104569792+Ov3r1ord04@users.noreply.github.com>
2025-03-10 23:04:36 -0700

    Login View

--- easystay_backend/users/urls.py
+++ easystay_backend/users/urls.py
@@ -3,5 +3,6 @@ from . import views
 app_name = "users"
 urlpatterns = [
     path('registration/', views.UserCreationView.as_view(), name='registration'),
-    path("login", views.UserLoginView.as_view(), name='login')
+    path("login", views.UserLoginView.as_view(), name='login'),
+    path("logout", views.logout, name='logout')
 ]
