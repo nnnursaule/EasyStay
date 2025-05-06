912655f5d9b8f6b732e9f021c0c97807ccb6a567
yera72934 <104569792+Ov3r1ord04@users.noreply.github.com>
2025-05-06 00:35:09 -0700

    booking with calendar, confim booking

--- easystay_backend/bookings/admin.py
+++ easystay_backend/bookings/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import Apartment, ResidentialComplex, Review, Favourite, Complaint
+from .models import Apartment, ResidentialComplex, Review, Favourite, Complaint, Feedback, Booking
 
 admin.site.register(Apartment)
 admin.site.register(ResidentialComplex)
 admin.site.register(Review)
 admin.site.register(Favourite)
 admin.site.register(Complaint)
+admin.site.register(Feedback)
+admin.site.register(Booking)
